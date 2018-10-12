2c19009451953af18b87a521fbb10f66ab387367
peter2233finn <c16407722@mydit.ie>
2018-10-12 16:17:42 +0100

    ff

---
+++ send_to_github.sh
@@ -0,0 +1,10 @@
+echo "enter short description of work done
+read message
+git add *
+git commit -m $message
+git push -u origin master
+echo "enter short description of work done
+read message
+git add *
+git commit -m $message
+git push -u origin master
