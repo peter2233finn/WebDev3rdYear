e53ce72224f0c05cb652e4fb08ab26ec594165c8
peter2233finn <c16407722@mydit.ie>
2018-11-14 07:31:42 +0000

    Update log.php

--- log.php
+++ log.php
@@ -1,45 +1,105 @@
+<!doctype html>
+
 <html>
-<body>
+<body background="linear-gradient(to bottom, #0033cc 0%, #0099ff 100%)">
+<link rel="stylesheet" type="text/css" href="CSS.css">
 
+ <div id="navbar">
+   <a href="index.html">Home</a>
+   <a href="kidsclub.html">Kids Club</a>
+   <a href="login.html">Book ahead</a>
+ </div>
+
+<head>
+<title>Luminous</title>
+</head>
+<div class = "background">
+
+<br>
+<br>
+<br>
+<br>
+
+<center>
+<h2>Create Account
+<br>
+<br>
 <?php
-$uname = $_POST["uname"];
-$pass = $_POST["pass"];
-$conn = mysqli_connect("localhost","bobbie","pug","webdev");
-
-if ($conn->connect_error)
-{
-	die("Connection failed: " . $conn->connect_error);
-}
-
-$result = $conn->query("SELECT Username FROM USER where Username = '$uname' and Password = '$pass'");
-if ($result->num_rows == 0)
-{
-        session_start();
-	$_SESSION['error_message'] = "Login failed. Try again.";log.php
-	header("Location: login.php");
-        die();
-}
-
-else
-{
-	session_start();
-	$_SESSION['error_message'] = "You are logged in";
-	header("Location: login.php");
-	die();
-	$conn->close();
-}
-
-function debug_to_console( $data ) {
-    $output = $data;
-    if ( is_array( $output ) )
-        $output = implode( ',', $output);
-
-    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
-}?>
+session_start();
+echo $_SESSION['error_message'];
+unset($_SESSION['error_message']);
+?>
+</h2>
 
 
 
+<table style="width:80%">
+  <form action="reg.php" method="post" style="width:100%; text-align:center">
+   <div class="container">
+
+   <tr>
+     <th><label for="fname"><b>Firstname</b></label></th>
+     <th><input type="text" placeholder="Enter firstname" name="fname" required></th>
+   </tr>
+
+    <br>
+    <br>
 
 
+   <tr>
+     <th><label for="lname"><b>Lastname</b></label></th>
+     <th><input type="text" placeholder="Enter lastname" name="lname" required></th>
+   </tr>
+    <br>
+    <br>
+
+
+   <tr>
+     <th><label for="email"><b>Email Address</b></label></th>
+     <th><input type="text" placeholder="Enter email" name="email" required></th>
+   </tr>
+    <br>
+    <br>
+
+   <tr>
+     <th><label for="uname"><b>Username</b></label></th>
+     <th><input type="text" placeholder="Enter Username" name="uname" required></th>
+   </tr>
+    <br>
+    <br>
+   <tr>
+     <th><label for="addss"><b>Address</b></label></th>
+     <th><input type="text" placeholder="Address" name="addss" required></th>
+   </tr>
+
+   <tr>
+     <th><label for="pass1"><b>Password</b></label></th>
+     <th><input type="password" placeholder="Enter password" name="pass1" required></th>
+   </tr>
+    <br>
+    <br>
+
+   <tr>
+     <th><label for="pass2"><b>Confirm Password</b></label></th>
+     <th><input type="password" placeholder="Confirm password" name="pass2" required></th>
+   </tr>
+    <br>
+    <br>
+</table>
+        
+     <button href="register.php" class="button" type="submit">Register</button>
+     <br>
+     <br>
+
+    </div>
+   <br>
+   <br>
+
+  </form>
+ </center>
+</div>
+<div class="footer"><p>Contact us<br><br>Got a question?</p>
 </body>
+
 </html>
+
