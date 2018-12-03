ea98a01ef9fee03c7dfbd157a9e1c42f77144908
peter2233finn <c16407722@mydit.ie>
2018-12-03 18:48:59 +0000

    Update header.php

--- header.php
+++ header.php
@@ -1,25 +1,29 @@
 <?php
 $toprint .= '<body id="bodyId"><div id="navbar"><a href="index.html">Home</a><a href="kidsclub.php">Kids Club</a>';
 $toprint .= '<link rel="stylesheet" type="text/css" href="CSS.css">';
+$toprint .= '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
+
+
 
 if (isset($_COOKIE["user_logged"]))
 {
-	$toprint .= '<a href="myaccount.php">My Account</a>';
-	$toprint .= '<a href="logout.php">Logout</a>';
+        $toprint .= '<a href="myaccount.php">My Account</a>';
+        $toprint .= '<a href="logout.php">Logout</a>';
 
-	if (isset($_COOKIE["Admin"]))
-	{
-		$toprint .= '<a href="admin.php">Administrator</a>';
-	}
+        if (isset($_COOKIE["Admin"]))
+        {
+                $toprint .= '<a href="admin.php">Administrator</a>';
+        }
 }
 else
 {
-	$toprint .= '<a href="login.php">Login</a>';
+        $toprint .= '<a href="login.php">Login</a>';
 }
+
 
 function logout()
 {
-	unset($_COOKIE["user_logged"]);
+        unset($_COOKIE["user_logged"]);
 }
 
 $toprint .= '</div>';
