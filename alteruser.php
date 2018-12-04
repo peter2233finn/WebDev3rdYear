29e2dfd6ed6ee5d4e8f49e79492ef2c117a72e0c
peter2233finn <c16407722@mydit.ie>
2018-12-04 00:47:22 +0000

    Create alteruser.php

---
+++ alteruser.php
@@ -0,0 +1,34 @@
+<?php
+include 'header.php';
+include 'SqlConnect.php';
+echo "<br><br><br><br><br><br><br><br><br>";
+echo "GAGAGAG";
+
+
+
+//echo var_dump($_POST);
+
+$pvar="";
+
+
+foreach ($_POST as $key => $value) 
+{
+	 $value;
+}
+
+
+
+
+
+if(isset($_POST["Delete"])) 
+{
+
+
+}
+
+if(isset($_POST["Movies"])) 
+{
+
+}
+include 'footer.php'
+?>
